1d4143b0d30d189e2111e2a16a1f7665fb510736
Volodymyr Vasylyna <143510572+VolodymyrVasylyna@users.noreply.github.com>
2024-10-23 16:23:44 +0300

    done

---
+++ guessing_game.py
@@ -0,0 +1,1 @@
+import turtle; def koch_curve(t, order, size): if order == 0: t.forward(size); else: for angle in [60, -120, 60, 0]: koch_curve(t, order-1, size/3); t.left(angle); turtle.speed(0); koch_curve(turtle, 4, 200); turtle.done()
